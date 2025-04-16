d0e4cf3399f2b01785f163d6be2ccd9c1c246a71
falckRepos <liseyPanda@users.noreply.github.com>
2025-04-16 19:46:10 +0000

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ CORS(app)  # ✅ Enable CORS
 
 # Elasticsearch
 ELASTICSEARCH_URL = 'http://elasticsearch:9200'
+KIBANA_URL = 'http://kibana-kompose-ndrc.apps.osc-trailer.trailer.ndrc.mil/app/dashboards#/view/f4c19b48-01c6-4fa2-bcac-64a87da4c652?_g=(refreshInterval:(pause:!t,value:60000),time:(from:now%2Fd,to:now%2Fd))&_a=()'
 
 # Cloud Database Connection
 def db_connection():
@@ -79,7 +80,7 @@ def get_truck_events():
 # ✅ Show Cloud Dashboard
 @app.route('/dashboard')
 def dashboard():
-    return render_template('dashboard.html')
+    return render_template('dashboard.html', kibana_url=KIBANA_URL)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
